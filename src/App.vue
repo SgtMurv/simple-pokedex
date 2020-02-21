c69f6cb489e5061eb1cf9971682fcdef5996b7ba
Matthew Honour <matthewpeterhonour@gmail.com>
2020-02-21 12:55:48 +0000

    called the api and displayed contents to the page

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 class="heading">(╯°□°)╯︵◓</h1>
+    <Pokemon
+      v-for="(pokemon, index) in pokemonList"
+      :key="index"
+      :name="pokemon.name"
+      :type="pokemon.type"
+    >
+    </Pokemon>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Pokemon from "./components/Pokemon";
+import axios from "axios"; // library to make HTTP requests
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  components: { Pokemon },
+  data: () => {
+    return {
+      pokemonList: [],
+      url: "https://pokeapi.co/api/v2/pokemon?limit=30&offset=151"
+    };
+  },
+  // lifecycle method
+  created() {
+    // fetch some data from the pokemon api.
+    this.getPokemon();
+  },
+  methods: {
+    getPokemon() {
+      axios.get(this.url).then(response => {
+        this.pokemonList = response.data.results;
+      });
+    }
   }
 };
 </script>
@@ -24,5 +47,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 500px;
+}
+.heading {
+  text-align: left;
+  margin-left: 170px;
 }
 </style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="pokemon">
+    <h2>
+      {{ name }}
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Pokemon",
+  props: {
+    name: String,
+    type: String
+  }
+};
+</script>
+
+<style>
+.pokemon {
+  padding: 10px;
+  border: 1px solid #808080;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  width: 30vw;
+}
+</style>
